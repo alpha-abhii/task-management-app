0bdd7c035bf8a2e0c5b46fb4ea3c94c96bd904ec
alpha-abhii <alpha20abhi@gmail.com>
2024-09-20 10:23:09 +0530

    Initial Setup

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import connectDB from './config/db';
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Task Manager API');
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
